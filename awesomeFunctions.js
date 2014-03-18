b9d184d9d058e42ffad36218e637f0dd97e715e0
nate dawg <nglass1988@gmail.com>
2014-03-18 00:29:21 -0700

    crockford memoize for factorial and fibonacci

--- awesomeFunctions.js
+++ awesomeFunctions.js
@@ -14,3 +14,26 @@ uniqueArray = myArray.filter(function(elem, pos) {
 _.max(_.keys(obj), function(key){ 
   return obj[key]["total_fans"]; 
 });
+
+// Douglas Crockford Memoizer for factorial and fibonacci
+function memoizer(memo, formula) {
+	var recur = function(n) {
+		var result = memo[n];
+		if (typeof result !== "number") {
+			result = formula(recur, n);
+			memo[n] = result;
+		}
+		return result;
+	}
+	return recur;
+}
+var factorial = memoizer([1,1], function(recur, n) {
+	return n * recur(n-1);
+});
+console.log(factorial(3));
+// formula(recur, 3) = 3 * recur(2) = formula(recur, 2) = 2 * recur(1) = 1
+var fibonacci = memoizer([0,1], function(recur, n) {
+	return recur(n-1) + recur(n+2);
+});
+console.log(fibonacci(3));
+// formula(recur, 3) = recur(2) = formula(recur, 2) = recur(1) = 1 + recur(0) = 0 + ( recur(1))
